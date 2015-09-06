09513ff26dc128b220d437734e227b84ed475ac3
emilyelaine <ecarey@mcad.edu>
2015-09-06 11:07:19 -0500

    linked external js page

---
+++ week2/assignment2/js/main.js
@@ -0,0 +1,36 @@
+
+            var alabama = document.getElementById('alabama');
+            var capitalFunction = function(){
+                alert('The capital of Alabama is Montgomery.');
+            };
+                alabama.addEventListener('mouseover',capitalFunction);
+
+            var alaska = document.getElementById('alaska');
+            var capitalFunction = function(){
+                alert('The capital of Alaska is Juneau.');
+            };
+                alaska.addEventListener('mouseover',capitalFunction);
+
+            var arizona = document.getElementById('arizona');
+            var capitalFunction = function(){
+                alert('The capital of Arizona is Phoenix.');
+            };
+                arizona.addEventListener('mouseover',capitalFunction);
+
+            var arkansas = document.getElementById('arkansas');
+            var capitalFunction = function(){
+                alert('The capital of Arkansas is Little Rock.');
+            };
+                arkansas.addEventListener('mouseover',capitalFunction);
+
+            var california = document.getElementById('california');
+            var capitalFunction = function(){
+                alert('The capital of California is Sacramento.');
+            };
+                california.addEventListener('mouseover',capitalFunction);
+
+            var colorado = document.getElementById('colorado');
+            var capitalFunction = function(){
+                alert('The capital of Colorado is Denver.');
+            };
+                colorado.addEventListener('mouseover',capitalFunction);  
