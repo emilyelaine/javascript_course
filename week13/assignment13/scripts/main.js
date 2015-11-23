de8ff7e6de55e3ce27c99d1ea765dbb4cfbd9274
emilyelaine <ecarey@mcad.edu>
2015-11-22 21:35:11 -0600

    Updated js for assignment 13

--- week13/assignment13/scripts/main.js
+++ week13/assignment13/scripts/main.js
@@ -1,44 +1,31 @@
- $('document').ready(function() {
+$(document).ready(function() {
 
-//hide answers on page load
-$(window).load(function() {
-	$('div.back img, div.back h3').hide();
-})
-
-//show specific state capital when user clicks a state
- $('.alabama div.front').click(function() {
- 	$('.alabama div.back img').eq($(this).index()).fadeIn('fast', 'linear');
- 	$('.alabama div.back h3').eq($(this).index()).fadeIn('fast', 'linear');
+//click on the card to flip and reveal the back side of the card
+$(function(){
+    $(".card").flip({
+        trigger: "click"
+    });
 });
 
-$('.alaska div.front').click(function() {
- 	$('.alaska div.back img').eq($(this).index()).fadeIn('fast', 'linear');
- 	$('.alaska div.back h3').eq($(this).index()).fadeIn('fast', 'linear');
+//highlight the card selected on hover
+$('.front').mouseover(function() {
+	$(this).css({
+			"border-color": "B22222", 
+            "border-weight": "5px", 
+            "border-style": "solid",
+            "border-radius": 25
+    });
 });
 
-$('.arizona div.front').click(function() {
- 	$('.arizona div.back img').eq($(this).index()).fadeIn('fast', 'linear');
- 	$('.arizona div.back h3').eq($(this).index()).fadeIn('fast', 'linear');
-});
+//remove highlight on mouseout
+$('.front').mouseout(function() {
+	$(this).css({
+			"border-color": "#0099CC",
+			"border-weight": "5px",
+			"border-style": "solid",
+			"border-radius": 25
+	});
 
-$('.arkansas div.front').click(function() {
- 	$('.arkansas div.back img').eq($(this).index()).fadeIn('fast', 'linear');
- 	$('.arkansas div.back h3').eq($(this).index()).fadeIn('fast', 'linear');
 });
-
-$('.california div.front').click(function() {
- 	$('.california div.back img').eq($(this).index()).fadeIn('fast', 'linear');
- 	$('.california div.back h3').eq($(this).index()).fadeIn('fast', 'linear');
-});
-
-$('.colorado div.front').click(function() {
- 	$('.colorado div.back img').eq($(this).index()).fadeIn('fast', 'linear');
- 	$('.colorado div.back h3').eq($(this).index()).fadeIn('fast', 'linear');
-});
-
-//hide the capital when clicked
- $('div.back').click(function() {
- 	$('div.back img, div.back h3').hide('fast');
- })
 
 });
