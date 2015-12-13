221707e69c0838667987f1f7790ee712bb6a4b6a
emilyelaine <ecarey@mcad.edu>
2015-12-12 19:43:14 -0600

    Added Nav Functionality
    Added nav functionality of sticky menu and animation on hover drop
    down.  Also corrected img slider width issue.

--- final/js/main.js
+++ final/js/main.js
@@ -29,7 +29,36 @@ function imgSlider() {
 	};
 
 $('document').ready(function() {
-	
-		setInterval('imgSlider()', 5000); 
+	setInterval('imgSlider()', 5000); 
 
+
+//sticky navigation to main nav
+var stickyNavTop = $('.mainNav').offset().top;
+ 
+var stickyNav = function(){
+var scrollTop = $(window).scrollTop();
+      
+if (scrollTop > stickyNavTop) { 
+    	$('.mainNav').addClass('sticky');
+	} else {
+    	$('.mainNav').removeClass('sticky'); 
+	}
+	};
+ 
+stickyNav();
+ 
+$(window).scroll(function() {
+    stickyNav();
+});
+
+
+//animate navigation
+$( '.dropdown' ).hover(
+        function(){
+            $(this).children('.submenu').slideDown(250);
+        },
+        function(){
+            $(this).children('.submenu').slideUp(250);
+        }
+    );
 });
